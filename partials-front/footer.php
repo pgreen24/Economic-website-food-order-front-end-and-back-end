aff371063086c384472b686416de0f1161227937
Rabeya Akter Lima <105388469+pgreen24@users.noreply.github.com>
2024-05-29 11:50:53 +0600

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/profile.php?id=100080011994842&mibextid=ZbWKwL"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://instagram.com/rabe7712?igshid=NzZlODBkYWE4Ng=="><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://bd.linkedin.com/in/rabeya-akter-lima-275381239"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Rabeya Akter Lima</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
